1cad2f0a6fd0370ac7c0092e808ef05a7072348a
vSriiman <68470931+A1mpk@users.noreply.github.com>
2026-05-17 19:57:05 -0400

    "Fixed Doctor AVailabitileis"

--- HopsitalSystem/Forms/DoctorAvailabilityForm.Designer.cs
+++ HopsitalSystem/Forms/DoctorAvailabilityForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.hopsitalDBDataSet = new HopsitalSystem.HopsitalDBDataSet();
             this.appointmentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.appointmentsTableAdapter = new HopsitalSystem.HopsitalDBDataSetTableAdapters.AppointmentsTableAdapter();
@@ -36,10 +37,6 @@
             this.lblDrName = new System.Windows.Forms.Label();
             this.scheduleGrid = new System.Windows.Forms.DataGridView();
             this.backButton = new System.Windows.Forms.Button();
-            this.removeButton = new System.Windows.Forms.Button();
-            this.saveButton = new System.Windows.Forms.Button();
-            this.updateButton = new System.Windows.Forms.Button();
-            this.addButton = new System.Windows.Forms.Button();
             this.DALbls = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.hopsitalDBDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).BeginInit();
@@ -71,20 +68,30 @@
             // 
             // lblDrName
             // 
-            this.lblDrName.AutoSize = true;
-            this.lblDrName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.lblDrName.Location = new System.Drawing.Point(41, 159);
+            this.lblDrName.Font = new System.Drawing.Font("Microsoft YaHei Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrName.Location = new System.Drawing.Point(3, 91);
             this.lblDrName.Name = "lblDrName";
-            this.lblDrName.Size = new System.Drawing.Size(64, 25);
+            this.lblDrName.Size = new System.Drawing.Size(773, 65);
             this.lblDrName.TabIndex = 2;
             this.lblDrName.Text = "label1";
+            this.lblDrName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // scheduleGrid
             // 
+            this.scheduleGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft YaHei Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.scheduleGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.scheduleGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.scheduleGrid.Location = new System.Drawing.Point(174, 159);
+            this.scheduleGrid.Location = new System.Drawing.Point(109, 159);
             this.scheduleGrid.Name = "scheduleGrid";
-            this.scheduleGrid.Size = new System.Drawing.Size(561, 357);
+            this.scheduleGrid.RowHeadersVisible = false;
+            this.scheduleGrid.Size = new System.Drawing.Size(550, 357);
             this.scheduleGrid.TabIndex = 3;
             // 
             // backButton
@@ -100,57 +107,6 @@
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
-            // removeButton
-            // 
-            this.removeButton.Font = new System.Drawing.Font("Microsoft YaHei Light", 17F);
-            this.removeButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.removeButton.Location = new System.Drawing.Point(14, 306);
-            this.removeButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(124, 46);
-            this.removeButton.TabIndex = 38;
-            this.removeButton.Text = "Remove";
-            this.removeButton.UseVisualStyleBackColor = true;
-            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
-            // 
-            // saveButton
-            // 
-            this.saveButton.Font = new System.Drawing.Font("Microsoft YaHei Light", 17F);
-            this.saveButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.saveButton.Location = new System.Drawing.Point(14, 358);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(124, 46);
-            this.saveButton.TabIndex = 37;
-            this.saveButton.Text = "Save";
-            this.saveButton.UseVisualStyleBackColor = true;
-            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
-            // 
-            // updateButton
-            // 
-            this.updateButton.Font = new System.Drawing.Font("Microsoft YaHei Light", 17F);
-            this.updateButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.updateButton.Location = new System.Drawing.Point(13, 254);
-            this.updateButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(124, 46);
-            this.updateButton.TabIndex = 36;
-            this.updateButton.Text = "Update";
-            this.updateButton.UseVisualStyleBackColor = true;
-            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
-            // 
-            // addButton
-            // 
-            this.addButton.Font = new System.Drawing.Font("Microsoft YaHei Light", 17F);
-            this.addButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.addButton.Location = new System.Drawing.Point(14, 202);
-            this.addButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(124, 46);
-            this.addButton.TabIndex = 35;
-            this.addButton.Text = "Add";
-            this.addButton.UseVisualStyleBackColor = true;
-            // 
             // DALbls
             // 
             this.DALbls.Font = new System.Drawing.Font("Microsoft YaHei Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -159,7 +115,7 @@
             this.DALbls.Name = "DALbls";
             this.DALbls.Size = new System.Drawing.Size(781, 57);
             this.DALbls.TabIndex = 40;
-            this.DALbls.Text = "Availabilities";
+            this.DALbls.Text = "Schedule";
             this.DALbls.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // DoctorAvailabilityForm
@@ -169,10 +125,6 @@
             this.ClientSize = new System.Drawing.Size(774, 584);
             this.Controls.Add(this.DALbls);
             this.Controls.Add(this.backButton);
-            this.Controls.Add(this.removeButton);
-            this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.updateButton);
-            this.Controls.Add(this.addButton);
             this.Controls.Add(this.scheduleGrid);
             this.Controls.Add(this.lblDrName);
             this.Name = "DoctorAvailabilityForm";
@@ -182,7 +134,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.scheduleGrid)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -195,10 +146,6 @@
         private System.Windows.Forms.Label lblDrName;
         private System.Windows.Forms.DataGridView scheduleGrid;
         private System.Windows.Forms.Button backButton;
-        private System.Windows.Forms.Button removeButton;
-        private System.Windows.Forms.Button saveButton;
-        private System.Windows.Forms.Button updateButton;
-        private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Label DALbls;
     }
 }
